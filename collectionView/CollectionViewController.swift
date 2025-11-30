949e19a1d1e2055dd72670b02bad12a552108441
AtesiT <atesit@mail.ru>
2025-11-30 18:30:53 +0300

    add newLine in our enum and made size cells by size of screen(and -50 pixels), also added 2 view controllers(and thier segues were named)

--- collectionView/CollectionViewController.swift
+++ collectionView/CollectionViewController.swift
@@ -3,11 +3,13 @@ import UIKit
 enum СountOfCellsWithSmth: CaseIterable {
     case phone
     case email
+    case fetchData
     
     var title: String {
         switch self {
         case .phone: return "Phone"
         case .email: return "Email"
+        case .fetchData: return "Fetch Data"
         }
     }
 }
@@ -28,5 +30,38 @@ final class CollectionViewController: UICollectionViewController {
         cell.label.text = allCells[indexPath.item].title
         
         return cell
+    }
+}
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    //  Данный метод возвращает размеры ячейки
+    //  CG - хранит данные о работе с графикой(числовые типа интерфейса)
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 50 , height: 100)
+    }
+    
+    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //  Метод, благодаря которому, можно отследить по какой ячейки нажимает пользователь
+        let userAction = allCells[indexPath.item]
+        
+        switch userAction {
+        case .phone:
+            performSegue(withIdentifier: "phoneSegue", sender: nil)
+        case .email:
+            performSegue(withIdentifier: "emailSegue", sender: nil)
+        case .fetchData:
+            print("")
+        }
+    }
+}
+
+extension CollectionViewController {
+    private func fetchData() {
+        
     }
 }
