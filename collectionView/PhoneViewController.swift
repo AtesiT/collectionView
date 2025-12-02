7b8b38980c60580385ace044a8abe4521a5dd4e2
AtesiT <atesit@mail.ru>
2025-12-02 12:45:21 +0300

    fix: added transfer from 2nd thread to main when we take a image. app is up

--- collectionView/PhoneViewController.swift
+++ collectionView/PhoneViewController.swift
@@ -17,6 +17,8 @@ final class PhoneViewController: UIViewController {
         // Data - содержатся данные, которые мы получаем после перехода по ссылке (фото)
         // Response - показывает различную информацию(по типу: откуда данные получены, метаданные)
         // Error - если есть data и response, то ошибки не будет
+        
+        // It will be not in the main thread at first
         URLSession.shared.dataTask(with: Link.phoneUrl.url) { [weak self] data, response, error in
             //  Делаем список захвата потому что, создаются две ссылки на этот VC и при закрытии одна ссылка оставалась бы в памяти
             guard let self else { return }
@@ -27,9 +29,14 @@ final class PhoneViewController: UIViewController {
             }
             print(response)
             
-            //  Инциализируем изображение (достаём изображение из data)
-            imageView.image = UIImage(data: data)
-            activityIndicator.stopAnimating()
+            //  We need return to main thread, because main thread has a UI logic
+            //  We also need return to main thread async (parallel to thread)
+            DispatchQueue.main.async {
+                //  Инциализируем изображение (достаём изображение из data)
+                //  Self - обработали уже, поэтому можно применять (без него Xcode показывает ошибку)
+                self.imageView.image = UIImage(data: data)
+                self.activityIndicator.stopAnimating()
+            }
         }.resume()
         //  Используем resume, потому что без вызова этого метода запрос(который выше) находится в подвешенном состоянии
     }
