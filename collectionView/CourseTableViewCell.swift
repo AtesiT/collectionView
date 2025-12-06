d3a60515851e98fc34e6830247f1ef73eb55264d
AtesiT <atesit@mail.ru>
2025-12-06 15:28:16 +0300

    replace some lines on the a method (as before) to display photo

--- collectionView/CourseTableViewCell.swift
+++ collectionView/CourseTableViewCell.swift
@@ -6,22 +6,17 @@ final class CourseTableViewCell: UITableViewCell {
     @IBOutlet var languageLabel: UILabel!
     @IBOutlet var idLabel: UILabel!
     
+    private let networkManager = NetworkManager.shared
+    
     func configure(with course: Course) {
         //  Весь этот код выполняется в основном потоке (main thread)
         nameLabel.text = course.name
         languageLabel.text = course.language
         idLabel.text = course.id
         //  Нужно выйти в фоновый поток (global)
-        DispatchQueue.global().async {
-            guard let imageData = try? Data(contentsOf: URL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png")!) else {return}
-            //  Нужно вернуться в основной поток, для того, чтобы отобразить UI
-            DispatchQueue.main.async() { [unowned self] in
-                //  Компилятор остановится и будет ждать, пока не сработает строка снизу (получение картинки с интернета)
-                //  После инициализации Data, содается изображение в imageData
-                //  Затем, мы передаем это изображение в наш Outlet
-                imageCourse.image = UIImage(data: imageData)
-            }
+        networkManager.fetchImage(from: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSci3e4K0DV-AzzuQdTfAqRL3ZXkjeQRnaWw&s")!) { [unowned self] imageData in
+            imageCourse.image = UIImage(data: imageData)
+            
         }
-        
     }
 }
